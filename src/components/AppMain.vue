682086fcc164e3be273c6c6cc4abe3302a856ca9
Piccio <lorenzo.hl.picchi@gmail.com>
2024-06-10 19:21:48 +0200

    main layout completed

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -15,16 +15,24 @@
     main{
         height: 100px;
         background-color: rgb(28, 28, 28);
+        padding-top: 1.5rem;
     }
 
     div.content{
         display: flex;
         padding: 1rem;
         color: white;
+        margin: 0 auto;
+        max-width: 80%;
+        text-align: center;
+        font-size: 1.3rem;
+        font-weight: 800;
+
     }
 
     div.content a{
         color: white;
         margin: 0 1rem;
+        text-decoration: none;
     }
 </style>
