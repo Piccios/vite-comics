6352fb193c24ae03016382fedb38dc263491b248
Piccio <lorenzo.hl.picchi@gmail.com>
2024-06-10 18:48:34 +0200

    set scss with correct folders and files

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -2,7 +2,30 @@
 
 </script>
 <template>
-    <h2>main</h2>
+    <main>
+        <div class="content">
+            <p> --> </p>
+            <a href="">Content goes here</a>
+            <p> <-- </p>
+        </div>
+    </main>
 </template>
 <style scoped> 
+
+    main{
+        height: 100px;
+        background-color: black;
+        
+    }
+
+    div.content{
+        display: flex;
+        padding: 1rem;
+        color: white;
+    }
+
+    div.content a{
+        color: white;
+        margin: 0 1rem;
+    }
 </style>
